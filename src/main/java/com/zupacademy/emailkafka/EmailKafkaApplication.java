9286e9d07716061c7378f1973afdfcc970bbca1c
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-10-15 15:54:34 -0300

    criacao projeto

---
+++ src/main/java/com/zupacademy/emailkafka/EmailKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.zupacademy.emailkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zupacademy/emailkafka/EmailKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zupacademy.emailkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
